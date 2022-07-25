b16b69be8987aa0a2ee8a9fa27fea4a1d55cb751
Shamanth <108264129+shamanth-deepsource@users.noreply.github.com>
2022-07-25 15:59:51 +0530

    # Commit to master 1

--- type_checks.py
+++ type_checks.py
@@ -6,36 +6,4 @@ if __name__ == "__main__":
     heights = [5.5, 6, 5.9]
     greet_all(heights)
 
-# Push event
-# Test PR 2
-# Test PR status draft
-# Push
-# Push webhook
-# Testing push event on default branch
-# Testing push event on default branch 2
-# Testing push event on default branch 3
-# Testing push event on default branch 4
-# Local commit + push
-# Local branch push
-# Default branch push test
-# Default branch push test - post permission update
-# Default branch push test - post permission update - all access
-# Back to square 1
-# Test commit to master
-# Test run with commit msg
-# Test run for PR 
-# commit 1 to master
-# commit 2 to master
-# commit 3 to master
-# commit 4 to master
-# PR 1 to master
-# commit to master 1
-# commit to master 2
-# commit to master 3
-# commit to master 4
-# commit to master 5
-# commit to master 6
-# commit to master 7
-# PR to master 1
-# PR to master - test
-# Commit to existing PR to master
+# Commit to master 1
