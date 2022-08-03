fbdb2b67f7377ed5cae43be08731b18bc6adde8e
Shamanth <108264129+shamanth-deepsource@users.noreply.github.com>
2022-08-03 22:30:43 +0530

    PR Status Update on Close

--- test_title.py
+++ test_title.py
@@ -1,13 +1,1 @@
-# Test Title
-# dummy
-# foo you
-# Test repo sync
-# Test PR Model error2
-# Test PR Model close2
-# Test PR Model Bug
-# Test PR Model select_related bug
-# Test PR Model fetch bug
-# Test PR Model - trigger analysis bug
-# Test PR Model - trigger analysis bug2
-# Test PR Model - trigger analysis: FIX
 # Test PR Model - trigger analysis: merged/closed
