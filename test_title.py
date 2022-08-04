b092a9c9dd65bb7e8e8371b2ec99c675669f94f3
Shamanth <108264129+shamanth-deepsource@users.noreply.github.com>
2022-08-04 11:44:12 +0530

    Test Draft PR

--- test_title.py
+++ test_title.py
@@ -1,17 +1,1 @@
-# Test Title
-# dummy
-# foo you
-# Test repo sync
-# Test PR Model error2
-# Test PR Model close2
-# Test PR Model Bug
-# Test PR Model select_related bug
-# Test PR Model fetch bug
-# Test PR Model - trigger analysis bug
-# Test PR Model - trigger analysis bug2
-# Test PR Model - trigger analysis: FIX
-# Test PR Model - trigger analysis: merged/closed
-# Test PR Model - sync_on_close
-# Test PR Model - sync_on_close - update PR object
-# Test PR Model - sync_on_close - update PR object2
-# Test PR Model - with self.pullrequests.filter instead
+# Test Draft PR
