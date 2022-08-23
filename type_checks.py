e5ad8e335a7a0115b20953ccbe3048054f6325a9
Shamanth <108264129+shamanth-deepsource@users.noreply.github.com>
2022-08-23 11:37:14 +0530

    pr - test issue count

--- type_checks.py
+++ type_checks.py
@@ -1,6 +1,7 @@
-# commit to master 1
-# commit to master 2
-# commit to master 3
+import os
+import sys
+
+
 def greet_all(names: list[str]) -> None:
     for name in names:
         print('Hello ' + name)
@@ -8,8 +9,3 @@ def greet_all(names: list[str]) -> None:
 if __name__ == "__main__":
     heights = [5.5, 6, 5.9]
     greet_all(heights)
-
-# Commit test 1
-# Commit to master
-# Commit to master 2
-# Test PR webhook payload
