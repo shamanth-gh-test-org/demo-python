c4fda8a41c16a10fbd3677d932b6e77a9d8e2949
Shamanth <108264129+shamanth-deepsource@users.noreply.github.com>
2022-08-29 12:21:08 +0530

    raise3

--- test_title.py
+++ test_title.py
@@ -1,6 +1,8 @@
-# Test Draft PR
-# Test ready_for_review
-# Test-Dev PR Creation
-# Testing on DEV
-# Testing on dev2
-# Test commit push
+#Raise 1
+# import os
+import sys
+def raises_issues(issues=[1,2,3]):
+    pass
+
+def greet_all(names: list[str]) -> None:
+    for name in names:
